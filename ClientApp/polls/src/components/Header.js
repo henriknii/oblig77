6889df5f3f51cb6a69452bc1939c3c70f42afd9c
henriknii <32524455+henriknii@users.noreply.github.com>
2020-11-06 23:09:42 +0100

    header og landingsside lagd.

---
+++ ClientApp/polls/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Header = () => {
+    return(
+    <div class="header-container">
+        <h1>Velkommen til</h1>
+        <section id ="yay-or-nay-container">
+            <p id="yay">Yay</p>
+            <p id="or">OR</p>
+            <p id="nay">Nay</p>
+        </section>
+    </div>
+    )
+}
+
+export default Header
