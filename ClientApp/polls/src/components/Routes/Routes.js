fa11c64560b1144093d21d1ecf442f8d1807be49
henriknii <32524455+henriknii@users.noreply.github.com>
2020-11-14 22:29:19 +0100

    Fixed
    Dynamically renders header and check it against the route.

--- ClientApp/polls/src/components/Routes/Routes.js
+++ ClientApp/polls/src/components/Routes/Routes.js
@@ -1,33 +1,38 @@
+import React, { useEffect, useState} from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
 
-import React from "react"
-import {Switch,Route,withRouter}  from 'react-router-dom';
 
 //Components
 import Login from "../Login/Login";
 import Header from "../Header";
-import Register from "../Register/Register"
-import Join from "../Join"
+import Register from "../Register/Register";
+import Join from "../Join";
+import PageNotFound from "../PageNotFound";
+
+//checking if PageNotFound is rendered so that the header dissapears.
+
+const Routes = ({ location }) => {
 
 
-const Routes = ({location}) => {
-    
-    const exlcudedPaths = [
-        '/registrer',
-        '/Create-poll'
 
-    ]
-return(
-        <div className="App">
-            <div className="col-sm-1 mx-auto">
-               {exlcudedPaths.indexOf(location.pathname) < 0 && <Header />}
-                <Switch>
-                <Route  path="/" exact component={Login} />
-                <Route path="/registrer" exact component={Register} />
-                <Route path="/join" exact component={Join} />
-                </Switch>
-            </div>
-        </div>
-)
-}
+  // Paths that header is excluded from.
+  const excludedPaths = ["/registrer", "/Create-poll"];
+
+
+
+  return (
+    <div className="App">
+      <div className="col-sm-1 mx-auto">
+        {excludedPaths.indexOf(location.pathname) < 0 &&  location.pathname !=="/404-page-not-found" && <Header />}
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/registrer" exact component={Register} />
+          <Route path="/join" exact component={Join} />
+          <Route component={PageNotFound}></Route>
+        </Switch>
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(Routes);
